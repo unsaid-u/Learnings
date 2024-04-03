db12bc3ee47c1c185049686d538b7172c802fd8e
Utkarsh Jain <utkarshjain@gofynd.com>
2024-04-03 15:53:04 +0530

    visited array optimization java

---
+++ java/basic.java
@@ -0,0 +1,18 @@
+public static void main(){
+    
+}
+
+/**
+ basic language understanding 
+    - syntax - 
+            - installation 
+            - file structure 
+            - data types and operators 
+            - functions, loops, fundamental inbuilts
+            - objects, classes - OOPs
+            - compilation, execution, memory management 
+            - exeception handling 
+            - process management 
+            - important libraries 
+ */
+
